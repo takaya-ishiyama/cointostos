524d49633e26bda15869501e1404bcb6ebc6ee64
takaya-ishiyama <takaya_ishiyama0810@yahoo.co.jp>
2024-11-09 23:59:03 +0900

    hoge

---
+++ frontend/src/app/cointoss/[id]/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+import * as coinConsts from '@/consts/coin'
+import React from 'react'
+
+export default function CoinToss() {
+  const [coin, setCoin] = React.useState<coinConsts.FrontAndBack | undefined>(undefined)
+
+  const handleClickCointoss = React.useCallback(() => {
+    const randomeZeroOrOne = getRandomInt(2)
+    console.log('\n-----------------------\n', randomeZeroOrOne)
+    switch (randomeZeroOrOne) {
+      case 0: {
+        setCoin(coinConsts.frontAndBack.front)
+        break
+      }
+      case 1: {
+        setCoin(coinConsts.frontAndBack.back)
+        break
+      }
+    }
+  }, [])
+
+  const handleReset = React.useCallback(() => {
+    setCoin(undefined)
+  }, [])
+
+  return (
+    <div>
+      <div >
+        <button onClick={handleClickCointoss} >
+          コイントス
+        </button>
+
+        <div>
+          <button onClick={handleReset} >
+            リセット
+          </button>
+        </div>
+      </div>
+      <div>
+        {`結果 : ${coin ?? 'まだコイントスをしていません'}`}
+      </div>
+    </div>
+  )
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
